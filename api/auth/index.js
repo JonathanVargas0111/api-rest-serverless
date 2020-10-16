9e699cba1a941a5a17530162ecb45b6d8bedc725
unknown <jonathanfelipe0111@hotmail.com>
2020-10-15 19:21:48 -0500

    Añadiendo configuracion error 502

--- api/auth/index.js
+++ api/auth/index.js
@@ -14,7 +14,6 @@ const isAuthenticated  = (req, res, next) =>{
             })
     })
 }
-
 const hasRoles = roles => (req, res, next)=>{
     if(roles.indexOf(req.user.role) > -1){
         return next()
